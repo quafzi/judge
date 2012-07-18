20c53f279b4edde72ed3272bbdc0010aedbcc878
Thomas Birke <thomas.birke@netresearch.de>
2012-07-18 10:52:59 +0200

    NAFQA-9: use parameters to detect possible incompatibilities

--- plugins/MageCompatibility/MageCompatibility.php
+++ plugins/MageCompatibility/MageCompatibility.php
@@ -64,6 +64,11 @@ class MageCompatibility implements JudgePlugin
         //TODO: FAIL if there are less than 3 latest versions supported
         //TODO: FAIL if the current version is not supported
         if (0 < $changesCount) {
+            Logger::addComment(
+                $extensionPath,
+                $this->name,
+                'Found ' . $changesCount . ' possible incompatibilities '
+            );
             Logger::setScore($extensionPath, $this->name, $settings->bad);
             return $settings->bad;
         }
@@ -112,27 +117,95 @@ class MageCompatibility implements JudgePlugin
             $direction = $direction=='>' ? 'upstream' : 'downstream';
             list ($token, $path, $codeLine, $type) = explode("\t", substr($line, 2));
             if ('f' == $type) {
-                /* look for changed function calls */
-                //$regexp = "->\W*$token\W*\([^,(]+(\(.*\))?(,[^,(]+(\(.*\))?){1,3}\)";
-                $regexp = "\->(\n|\W)*$token(\n|\W)*\((\n|.)*\)";
-                $command = 'pcregrep -rMc --files-with-matches "' . $regexp . '" ' . $extensionPath . '/app';
-                $command = 'grep -rEl "' . $token . '" ' . $extensionPath . '/app';
-                exec($command, $matches, $return);
-                foreach ($matches as $filePath) {
-                    $content = file_get_contents($filePath);
-                    preg_match('/' . $regexp . '/mU', $content, $detailedMatches);
-                    if (count($detailedMatches)) {
-                        $filePath = str_replace($extensionPath, '', $filePath);
-                        $changes[$direction][] = array(
-                            'type'  => $type,
-                            'file'  => $filePath,
-                            'count' => count($detailedMatches)
-                        );
-                    }
-                }
+                $changes = array_merge_recursive(
+                    $changes,
+                    $this->findDeprecatedFunction($extensionPath, $direction, $token, $codeLine, $type)
+                );
             }
         }
         $result['changes'] = $changes;
         return $result;
+    }
+
+    protected function findDeprecatedFunction($extensionPath, $direction, $token, $codeLine, $type)
+    {
+        $changes = array(
+            'upstream'   => array(),
+            'downstream' => array()
+        );
+
+        /* find extension files including a call of a method with that name */
+        $command = 'grep -rEl "' . $token . '" ' . $extensionPath . '/app';
+        exec($command, $usedInFile, $return);
+
+        if (0 == count($usedInFile)) {
+            return $changes;
+        }
+
+        /* analyze method params */
+        preg_match('/function\W+(.*\))/', $codeLine, $matches);
+        $call = $matches[1];
+        $paramsCount = $this->getCountOfParams($call);
+
+        /* look for changed function calls, regarding its params */
+        $regexp = "\->(\n|\W)*$token(\n|\W)*\(";
+        $regexp .= $this->getRegexpForParams($paramsCount['required'], $paramsCount['optional']);
+        $regexp .= "\)";
+        foreach ($usedInFile as $filePath) {
+            $content = file_get_contents($filePath);
+            preg_match('/' . $regexp . '/mU', $content, $detailedMatches);
+            if (count($detailedMatches)) {
+                $filePath = str_replace($extensionPath, '', $filePath);
+                $changes[$direction][] = array(
+                    'type'  => 'f',
+                    'file'  => $filePath,
+                    'count' => count($detailedMatches)
+                );
+            }
+        }
+        return $changes;
+    }
+
+    protected function getRegexpForParams($required, $optional)
+    {
+        $requiredParam = '[^,(]+(\(.*\))?';
+        $optionalParam = '(,(\n|\W)*' . $requiredParam . ')?';
+
+        $regexpRequiredParams = '';
+        if (0 < $required) {
+            $regexpRequiredParams = implode(',(\n|\W)*', array_fill(0, $required, $requiredParam));
+        }
+        $regexpOptionalParams = str_repeat($optionalParam, $optional);
+
+        if (0 == $required && 0 < $optional) {
+            /* strip first "," if there were no parameters before first optional parameter */
+            $regexpOptionalParams = '(' . substr($regexpOptionalParams, 2);
+        }
+
+        $regexp = $regexpRequiredParams . $regexpOptionalParams;
+    }
+
+    protected function getCountOfParams($call)
+    {
+        $count = array(
+            'required' => 0,
+            'optional' => 0
+        );
+        preg_match('/(.*)\((.*)\)/', $call, $matches);
+        list ($call, $method, $params) = $matches;
+
+        if (strlen($params)) {
+            $params = explode(', ', $params);
+            $countOfRequiredParams = 0;
+            $countOfOptionalParams = 0;
+            foreach ($params as $param) {
+                if (false !== strpos($param, '=')) {
+                    $count['required']++;
+                } else {
+                    $count['optional']++;
+                }
+            }
+        }
+        return $count;
     }
 }
