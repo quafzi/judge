6418f4d4ded2111627571f27ed879387b4684843
Michael Luehr <michael.luehr@netresearch.de>
2012-07-20 11:18:15 +0200

    NAFQA-4 add phpcpd, phpdepend to SourceCodeComplexity

--- plugins/SourceCodeComplexity/SourceCodeComplexity.php
+++ plugins/SourceCodeComplexity/SourceCodeComplexity.php
@@ -19,64 +19,90 @@ class SourceCodeComplexity implements JudgePlugin
         $this->settings = $this->config->plugins->{$this->name};
     }
 
+    /**
+     *
+     * @param string $extensionPath the path to the extension to check
+     * @return float the sum of scores of all tests
+     */
     public function execute($extensionPath)
     {
-        $metricViolations = 0;
         $score = 0;
-        $results = $this->executePHPDepend($extensionPath);
-        foreach ($results as $metric => $value) {
-            if ($this->isCriticalMetricValue($metric, $value)) {
+        $score = $score + $this->executePHPDepend($extensionPath);
+        $score = $score + $this->executePHPCpd($extensionPath);
+        $score = $score + $this->executePHPMessDetector($extensionPath);
+        Logger::setScore($extensionPath, $this->name, $score);
+        return $score;
+    }
+
+    /**
+     * checks the extension with phpMessDetector and returns the scoring
+     *
+     * @param string $extensionPath extension to check
+     * @return float the scoring for the extension after php mess detector test
+     */
+    protected function executePHPMessDetector($extensionPath)
+    {
+        $executable = 'plugins/' . $this->name . '/lib/PHP/Depend/src/bin/phpmd';
+        $score = $this->settings->phpMessDetector->good;
+        $mdResults = array();
+        exec(sprintf($executable . ' "%s" "%s" "%s"', $extensionPath, 'text', $this->settings->phpMessDetector->useRuleSets), $mdResults);
+        if ($this->settings->phpMessDetector->allowedIssues < count($mdResults)) {
+            $score = $this->settings->phpMessDetector->bad;
+            foreach ($mdResults as $issue) {
+                Logger::addComment($this->name, 'failed', $issue);
+            }
+        }
+        return $score;
+    }
+
+    /**
+     * checks the extensions complexity with phpDepend and returns the scoring
+     *
+     * @param string $extensionPath extension to check
+     * @return float the scoring for the extension after php depend test
+     */
+    protected function executePHPDepend($extensionPath)
+    {
+        $executable = 'plugins/' . $this->name . '/lib/PHP/Depend/src/bin/phpdepend';
+        $metricViolations = 0;
+        $tempXml = $this->settings->phpDepend->tmpXmlFilename;
+        $usedMetrics = $this->settings->phpDepend->useMetrics->toArray();
+        exec(sprintf($executable . ' --summary-xml="%s" "%s"', $tempXml, $extensionPath));
+        $metrics = current(simplexml_load_file($tempXml));
+        foreach ($metrics as $metricName => $metricValue) {
+            if (in_array($metricName, $usedMetrics)
+                && $this->settings->phpDepend->{$metricName} < $metricValue) {
                 Logger::addComment(
                     $this->extensionPath,
                     $this->name,
-                    '<comment>Critical metric ' . $metric . ' value: ' . $value . '</comment>');
+                    '<comment>Critical metric ' . $metricName . ' value: ' . $metricValue . '</comment>');
                 ++ $metricViolations;
             }
         }
-        $score = $this->settings->metricViolations->good;
-        if ($this->settings->metricViolations->allowedMetricViolations < $metricViolations) {
-            $score = $score + $this->settings->metricViolations->bad;
+        $score = $this->settings->phpDepend->metricViolations->good;
+        if ($this->settings->phpDepend->metricViolations->allowedMetricViolations < $metricViolations) {
+            $score = $score + $this->settings->phpDepend->metricViolations->bad;
         }
         Logger::success('%d metric violations found in %s', array($metricViolations, $extensionPath));
-
-
-        $score = $score + $this->executePHPCpd($extensionPath);
-
-        Logger::setScore($extensionPath, $this->name, $score);
+        unlink($tempXml);
         return $score;
-
-//        $results = $this->executePHPMessDetection($extensionPath);
-//        var_dump($results);
-//        $score = 0;
-//        Logger::setScore($extensionPath, $this->name, $score);
-//        return $score;
     }
 
-    protected function executePHPMessDetection($extensionPath)
-    {
-        $mdResults = array();
-        exec(sprintf('phpmd "%s" "%s" "%s"', $extensionPath, 'text', 'codesize'), $mdResults);
-        return $mdResults;
-    }
-
-
-    protected function executePHPDepend($extensionPath)
-    {
-        $pdResults = array();
-        $executable = 'plugins/' . $this->name . '/lib/PHP/Depend/src/bin/pdepend';
-        exec(sprintf($executable . ' --"%s" "%s"', 'summary-xml=sum.xml', $extensionPath));
-        $xml = current(simplexml_load_file('sum.xml'));
-        return $xml;
-    }
-
+    /**
+     *  checks the extension with php copy and paste detector
+     *
+     * @param string $extensionPath extension to check
+     * @return float the scoring for the extension after phpcpd test
+     */
     protected function executePHPCpd($extensionPath)
     {
+        $executable = 'plugins/' . $this->name . '/lib/PHP/Depend/src/bin/phpcpd';
         $line = '';
         $cpdPercentage = 0;
         $scoreForPhpCpd = $this->settings->phpcpd->good;
         $minLines   = $this->settings->phpcpd->minLines;
         $minTokens  = $this->settings->phpcpd->minTokens;
-        exec(sprintf('phpcpd --min-lines "%s" --min-tokens "%s" --quiet "%s"', $minLines, $minTokens, $extensionPath), $output);
+        exec(sprintf($executable . ' --min-lines "%s" --min-tokens "%s" --quiet "%s"', $minLines, $minTokens, $extensionPath), $output);
         foreach ($output as $line) {
             if (false !== strpos($line, '% duplicated')) {
                 break;
@@ -88,17 +114,4 @@ class SourceCodeComplexity implements JudgePlugin
         }
         return $scoreForPhpCpd;
     }
-
-    protected function isCriticalMetricValue($metricName, $value)
-    {
-        $result = false;
-        if (in_array($metricName, $this->settings->phpdepend->useMetrics->toArray())
-            && $this->settings->phpdepend->{$metricName} < $value) {
-            $result = true;
-        }
-        return $result;
-    }
-
 }
-
-
