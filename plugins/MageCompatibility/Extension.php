dbf8fce598be171fc0976f44b2e7b691be2d5607
Thomas Birke <thomas.birke@netresearch.de>
2012-09-03 10:44:59 +0200

    NAFQA-20: skip binary files method detection

--- plugins/MageCompatibility/Extension.php
+++ plugins/MageCompatibility/Extension.php
@@ -472,6 +472,9 @@ class Extension extends Config
             $command = sprintf( 'grep -oriE " function ([a-zA-Z0-9_]*)" %s', $this->extensionPath . '/app/code/');
             exec($command, $output);
             foreach ($output as $line) {
+                if (false === strpos(':', $line)) {
+                    continue;
+                }
                 list($path, $method) = explode(':', $line);
                 $this->methods[trim(str_replace('function', '', $method))] = trim(substr_replace($this->extensionPath, '', $path));
             }
